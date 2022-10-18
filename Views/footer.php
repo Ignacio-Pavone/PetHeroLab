0578d13d27c44e6362e8d6fb5f9b1f2ef1cee8fe
Ignacio Pavone <80071197+Ignacio-Pavone@users.noreply.github.com>
2022-10-18 09:00:01 -0300

    add date

--- Views/footer.php
+++ Views/footer.php
@@ -1,5 +1,5 @@
 </body>
 <br>
 <br>
-<h4 id = "idFoot" style = "text-align: center;">Desarrollado por Nazareno Rodriguez, Taliercio Luis, Pavone Ignacio - UTN Mar Del Plata</h4>
+<h4 id = "idFoot" style = "text-align: center;">Desarrollado por Nazareno Rodriguez, Taliercio Luis, Pavone Ignacio - UTN Mar Del Plata - <?php echo date('Y'); ?></h4>
 </html>
