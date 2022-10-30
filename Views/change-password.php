7008e89a832ec073f0ef3b350f4a1e68e12386b4
Ignacio Pavone <80071197+Ignacio-Pavone@users.noreply.github.com>
2022-10-29 21:14:48 -0300

    :white_check_mark: add check both password in modify pass

--- Views/change-password.php
+++ Views/change-password.php
@@ -42,20 +42,20 @@ include('nav-simple-bar.php');
 
             <div class="col-lg-4 input-group mb-3">
                 <div class="input-group-prepend">
-                    <span class="btn btn-md btn-dark m-0 px-3" id="basic-addon1">Nueva Contraseña</span>
+                    <span class="btn btn-md btn-dark m-0 px-3" id="basic-addon1">Nueva</span>
                 </div>
-                <input type="password" class="form-control" placeholder="****" name="newPassword"
+                <input type="password" class="form-control" id = "newPassword" placeholder="****" name="newPassword"
                 aria-describedby="basic-addon1" required>
             </div>
 
             <div class="col-lg-4 input-group mb-3">
                 <div class="input-group-prepend">
-                    <span class="btn btn-md btn-dark m-0 px-3" id="basic-addon1">Nueva Contraseña</span>
+                    <span class="btn btn-md btn-dark m-0 px-3" id="basic-addon1">Nueva</span>
                 </div>
-                <input type="password" class="form-control" placeholder="****" name="newPassword2"
+                <input type="password" class="form-control" id = "newPassword2" placeholder="****" name="newPassword2"
                 aria-describedby="basic-addon1" required>
             </div>
-            
+            <span id='message' style="margin-left: 500px"></span>           
             <div class="row" id="buttonraro" style="border: 1px solid">
                 <div class="col-lg-1" style="text-align:center">
                     <button type="submit" onclick="return confirm('Are you sure?')"
@@ -67,6 +67,14 @@ include('nav-simple-bar.php');
             <br>
 
         </section>
-
     </main>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script>
+        $('#newPassword, #newPassword2').on('keyup', function () {
+        if ($('#newPassword').val() == $('#newPassword2').val()) {
+            $('#message').html('Coinciden').css('color', 'green');
+        } else 
+            $('#message').html('No Coinciden').css('color', 'red');
+        });
+    </script>
 
